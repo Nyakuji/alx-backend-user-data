e6fce50c4506a3e674162d29f3f9b47026cdede8
philip Opiyo <opiyophilipo@gmail.com>
2024-06-04 10:24:28 +0300

    Request validation! and Basic auth Implementation

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -53,7 +53,9 @@ class Auth:
         Returns:
         - str: The authorization header.
         """
-        return None
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers['Authorization']
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
