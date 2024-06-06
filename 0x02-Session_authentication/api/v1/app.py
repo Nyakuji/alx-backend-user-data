368a5cdd0fa5620728d2181606f72740657a6142
philip Opiyo <opiyophilipo@gmail.com>
2024-06-06 10:48:04 +0300

    refactor: Add new path to request_path_list in before_request()
    This commit adds a new path '/api/v1/auth_session/login/' to the `request_path_list` in the `before_request()` function of `app.py`. This ensures that the authentication check is performed for this path as well, preventing unauthorized access. The change also includes a condition to check both the absence of an authorization header and a session cookie before aborting with a 401 error. This improves the security of the session authentication process.

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -57,10 +57,13 @@ def before_request() -> str:
     request_path_list = [
         '/api/v1/status/',
         '/api/v1/unauthorized/',
-        '/api/v1/forbidden/']
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/']  # Add the new path here
     if auth:
         if auth.require_auth(request.path, request_path_list):
-            if auth.authorization_header(request) is None:
+            if auth.authorization_header(request) is \
+                None and auth.session_cookie(
+                    request) is None:  # Check both conditions
                 abort(401)
             request.current_user = auth.current_user(request)
             if request.current_user is None:
