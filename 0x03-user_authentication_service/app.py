3c03c05af6a3a1b5aab4d1215fd177607ae73148
philip Opiyo <opiyophilipo@gmail.com>
2024-06-11 14:00:53 +0300

    Implemented Register user in app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,14 +1,31 @@
 #!/usr/bin/env python3
 """app module"""
 from flask import Flask, jsonify
+from auth import Auth
+from flask import request
+from typing import Dict, Union, Tuple
+
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"])
 def welcome() -> str:
     """Returns a JSON payload with a welcome message"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def register_user() -> Union[Dict[str, str], Tuple[Dict[str, str], int]]:
+    """Registers a new user"""
+    try:
+        email = request.form.get("email")
+        password = request.form.get("password")
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
