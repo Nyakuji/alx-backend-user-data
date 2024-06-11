1449d65973797e5af802fbddda187cec20d7de21
philip Opiyo <opiyophilipo@gmail.com>
2024-06-11 14:16:20 +0300

    Refactor UUID generation to separate function

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -23,6 +23,16 @@ def _hash_password(password: str) -> bytes:
     return hashed_password
 
 
+def _generate_uuid() -> str:
+    """Generates a new UUID.
+
+    Returns:
+        A string representation of the new UUID.
+
+    """
+    return str(uuid.uuid4())
+
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
@@ -72,12 +82,3 @@ class Auth:
                 return False
         except NoResultFound:
             return False
-
-    def _generate_uuid() -> str:
-        """Generates a new UUID.
-
-        Returns:
-            A string representation of the new UUID.
-
-        """
-        return str(uuid.uuid4())
