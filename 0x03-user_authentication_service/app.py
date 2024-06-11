e88a506cd629719e8d9997cdea01f4cdf0ac6810
philip Opiyo <opiyophilipo@gmail.com>
2024-06-11 14:31:57 +0300

    implemented login using flask

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """app module"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort
 from auth import Auth
-from flask import request
 from typing import Dict, Union, Tuple
 
 
@@ -26,6 +25,22 @@ def register_user() -> Union[Dict[str, str], Tuple[Dict[str, str], int]]:
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"])
+def login() -> Union[Dict[str, str], Tuple[Dict[str, str], int]]:
+    """Logs in a user"""
+    try:
+        email = request.form.get("email")
+        password = request.form.get("password")
+        if not AUTH.valid_login(email, password):
+            abort(401)
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie("session_id", session_id)
+        return response
+    except ValueError:
+        abort(401)
 
 
 if __name__ == "__main__":
