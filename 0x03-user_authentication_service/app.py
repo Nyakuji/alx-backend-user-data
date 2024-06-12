b36fa9b1846069ee3aa890e2cc6e9db617a802ca
philip Opiyo <opiyophilipo@gmail.com>
2024-06-12 12:49:10 +0300

    implemented Get reset password token

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -69,5 +69,16 @@ def profile() -> Union[Dict[str, str], Tuple[Dict[str, str], int]]:
     return jsonify({"email": user.email}), 200
 
 
+@app.route("/reset_password", methods=["POST"])
+def get_reset_password_token(
+) -> Union[Dict[str, str], Tuple[Dict[str, str], int]]:
+    """Generates a reset password token"""
+    email = request.form.get("email")
+    if not AUTH.find_user_by(email=email):
+        abort(403)
+    reset_token = AUTH.get_reset_password_token(email)
+    return jsonify({"email": email, "reset_token": reset_token}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
